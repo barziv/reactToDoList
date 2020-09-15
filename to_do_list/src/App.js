25f163e0ce3c0de5559b44aeead0c9cd7b46e6b8
bar ziv <barziv65@gmail.com>
2020-09-15 18:53:34 +0300

    main page is manage the data

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AssignmentsList from "./Components/List";
+import Header from "./Components/Header";
+import DownButtons from "./Containers/buttons";
 
 function App() {
+  const [assignments, setAssignments] = useState({});
+
+  const changeAssignment = (id, isDone, label) => {
+    setAssignments({...assignments,
+      [id]: {
+        'done': isDone,
+        'assignment': label
+      }
+    });
+  };
+
+  const deleteAssignment = id => {
+    setAssignments(curr => {
+      delete curr[id];
+      let newAssignments = {};
+      Object.assign(newAssignments, curr);
+      return newAssignments;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          To Do List
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>To Do List</Header>
+      <AssignmentsList
+        data={assignments}
+        change={changeAssignment}
+        delete={deleteAssignment}
+      />
+      <DownButtons add={changeAssignment} />
     </div>
   );
 }
