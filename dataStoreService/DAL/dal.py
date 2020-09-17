815ab7b67e4a26b1163369c6a85e66ccafb56545
bar ziv <barziv65@gmail.com>
2020-09-17 17:05:46 +0300

    mongo dal class

---
+++ dataStoreService/DAL/dal.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from bson import ObjectId
+
+class MongoDal():
+    def __init__(self, dbAddress):
+        client = MongoClient(dbAddress)
+        self.db = client.toDoList.toDoList
+
+    def getData(self, id):
+        id = ObjectId(id)
+        data = self.db.find_one({'_id': id})
+        del data["_id"]
+        return data
+
+    def deleteData(self, id):
+        self.db.delete_one({'_id': ObjectId(id)})
+
+    def insertData(self, jsonData, id=None):
+        if(id):
+            self.deleteData(id)
+        
+        result = self.db.insert_one(jsonData)
+        return (True, result.inserted_id)
