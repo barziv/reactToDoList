a41849413839988a120117cf6b834eb813aa7182
bar ziv <barziv65@gmail.com>
2020-09-17 22:37:31 +0300

    mongo server uri changed

--- dataStoreService/config.py
+++ dataStoreService/config.py
@@ -1,5 +1,5 @@
-PORT = 5000
-DAL_URL = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb"
+PORT = 5000 
+DAL_URL = "mongodb+srv://bar:Password1@todolist.3zpqm.mongodb.net/toDoList?retryWrites=true&w=majority"
 COOKIE_ID = "ListID"
 CORS_HEADERS = 'Content-Type'
 ACCESS_CONTROL_ALLOW_ORIGIN = 'http://localhost:3000'
