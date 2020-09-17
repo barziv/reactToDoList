02e779277fb6d7e8baf03baf09c16f3195c964b8
bar ziv <barziv65@gmail.com>
2020-09-17 22:16:12 +0300

    flask app communicate with react app

--- dataStoreService/main.py
+++ dataStoreService/main.py
@@ -1,23 +1,30 @@
 from flask import Flask, make_response, jsonify, request
+from flask_cors import CORS, cross_origin
+import json
 import config
 import DAL.dal
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = config.CORS_HEADERS
+cors = CORS(app)
+
 dal = DAL.dal.MongoDal(config.DAL_URL)
 
 @app.route('/save', methods=['POST'])
 def saveToDoList():
-    id = request.cookies.get('id')
-    (isSucceed, id) = dal.insertData(request.json, id)
-    res = jsonify(success=isSucceed)
-    res.set_cookie('id', str(id))
+    id = request.cookies.get(config.COOKIE_ID)
+    (isSucceed, id) = dal.insertData(json.loads(request.data), id)
+    res = jsonify(success=True)
+    res.set_cookie(config.COOKIE_ID, str(id))
     return res
 
 @app.route('/load', methods=['GET'])
 def loadToDoList():
-    id = request.cookies.get('id')
+    id = request.cookies.get(config.COOKIE_ID)
     data = dal.getData(id)
-    return jsonify({'list': data})
+    response = make_response(jsonify(data), 200)
+    response.headers = config.LOAD_RESPONSE_HEADERS
+    return response
 
 if __name__ == '__main__':
-    app.run(port="5000", threaded=True, debug=True)
+    app.run(port=config.PORT, threaded=True, debug=True)
