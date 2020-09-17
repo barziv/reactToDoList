e5b85437fb5aea293748d35a09e138d89bef5fe6
bar ziv <barziv65@gmail.com>
2020-09-17 22:16:56 +0300

    export data to config file

--- dataStoreService/config.py
+++ dataStoreService/config.py
@@ -1,1 +1,9 @@
+PORT = 5000
 DAL_URL = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb"
+COOKIE_ID = "ListID"
+CORS_HEADERS = 'Content-Type'
+ACCESS_CONTROL_ALLOW_ORIGIN = 'http://localhost:3000'
+LOAD_RESPONSE_HEADERS = {
+    'Access-Control-Allow-Origin': ACCESS_CONTROL_ALLOW_ORIGIN,
+    'Access-Control-Allow-Credentials': 'true'
+}
